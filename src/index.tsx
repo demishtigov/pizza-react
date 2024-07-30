9c90d6c5c4fe3893fc934e6bc034aea4fff19bfc
demishtigov <magomed.demishtigov@yandex.ru>
2024-07-30 14:14:27 +0300

    fixed some issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-const rootElem = document.getElementById('root');
+const rootElem = document.getElementById("root");
 
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
@@ -13,6 +13,6 @@ if (rootElem) {
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>,
+    </BrowserRouter>
   );
 }
